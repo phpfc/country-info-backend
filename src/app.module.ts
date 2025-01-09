280d90f6a2521563c9e383f258491fe41e877171
PedroFranca2299 <pedrofranca-tech@proton.me>
2025-01-09 09:09:04 -0300

    feat: configure app module with countries feature

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CountriesModule } from './countries/countries.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CountriesModule,
+  ],
+})
+export class AppModule { }
